e6b8d1e21db149d1c1da876d63a22fd1cafac42f
Unknown <jerrysun1996@gmail.com>
2021-10-12 01:30:16 -0400

    lab3

---
+++ Assets/_Scripts/BulletController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+
+    public float speed = 10; 
+    public float verticalBoundary = 5;
+    public BulletManager bulletManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletManager = FindObjectOfType<BulletManager>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _Move();
+        _CheckBounds();
+    }
+
+    void _Move()
+    {
+        transform.Translate(new Vector3(0,speed*Time.deltaTime,0));
+    }
+
+    private void _CheckBounds()
+    {
+        if(transform.position.y > verticalBoundary)
+        {
+            bulletManager.ReturnBullet(this.gameObject); 
+        }
+    }
+}
---
+++ Assets/_Scripts/BulletManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BulletManager : MonoBehaviour
+{
+    public GameObject bullet;
+    public int maxBullets; 
+    // Start is called before the first frame update
+    private Queue<GameObject> _bulletPool;
+
+    void Start()
+    {
+        _GenerateBullets();
+    }
+
+    private void _GenerateBullets()
+    {
+        _bulletPool = new Queue<GameObject>();
+        for (int count = 0; count < maxBullets; count++)
+        {
+            GameObject tempBullet = Instantiate(bullet);
+            tempBullet.SetActive(false);
+            tempBullet.transform.parent = transform;
+            _bulletPool.Enqueue(tempBullet);
+        }
+    }
+
+    public GameObject GetBullet(Vector3 position)
+    {
+        GameObject newBullet = _bulletPool.Dequeue();
+        newBullet.SetActive(true);
+        newBullet.transform.position = position;
+        return newBullet;
+    }
+
+    public void ReturnBullet(GameObject returnedBullet)
+    {
+        returnedBullet.SetActive(false);
+        _bulletPool.Enqueue(returnedBullet);
+    }
+}
